dd03037e3d4537224824dfd01480aac7fa9c5e78
Ariel Huang <ariel92911@gmail.com>
2019-10-22 23:34:00 +0800

    Add index template

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  console.log('get form POST request')
+  res.render('index')
+})
+
 // starts the express server and listening for connections.
 app.listen(port, () => {
   console.log(`Express app listening on port ${port}.`)
